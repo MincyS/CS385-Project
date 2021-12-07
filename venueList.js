1ef8ad4fa7b038f603bb727025d8c4a22d611ed6
mincy80 <91506101+mincy80@users.noreply.github.com>
2021-12-07 23:55:08 +0000

    Create venueList.js

---
+++ venueList.js
@@ -0,0 +1,38 @@
+export const venueList = [
+  {
+    id: 1,
+    venuename: "The Clubhouse at Fota Island Resort, Cork",
+    capacity: 220,
+    pricepp: 80
+  },
+  {
+    id: 2,
+    venuename: "Ballyseede Castle Hotel, Kerry",
+    capacity: 100,
+    pricepp: 75
+  },
+  {
+    id: 3,
+    venuename: "Springfort Hall Hotel and Country House, Cork",
+    capacity: 340,
+    pricepp: 70
+  },
+  {
+    id: 4,
+    venuename: "Faithlegg House Hotel, Waterford",
+    capacity: 250,
+    pricepp: 60
+  },
+  {
+    id: 5,
+    venuename: "Glenview Hotel and Leisure Club, Wicklow",
+    capacity: 150,
+    pricepp: 90
+  },
+  {
+    id: 6,
+    venuename: "Radisson Blu St Helens, Dublin",
+    capacity: 280,
+    pricepp: 85
+  }
+];
